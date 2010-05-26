50ddd654a09f48e769a9db2cd62b74d8618f83ce
aksenoff.a <aksenoff@users.noreply.github.com>
2010-05-26 23:50:49 +0000

    minor optimizations

--- model/model.h
+++ model/model.h
@@ -8,7 +8,7 @@ public:
 	Tree *t;
 	bool escaped, newTree, nodeAdded, rootModified;
 	Model();
-	bool get_symbol(short, SYMBOL*);
+	void get_symbol(short, SYMBOL*);
 	void get_scale(SYMBOL*);
     short get_byte(unsigned short, SYMBOL*);
 	static void encode_in_null_table(BYTE d, SYMBOL* s);
